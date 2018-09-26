0b7823aaf4e36b7505b62c3c8a286186d2a27a26
huangyt39 <824551061@qq.com>
2018-09-26 23:48:34 +0800

    9.26

---
+++ 18.py
@@ -0,0 +1,34 @@
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, nums):
+        # write your code here
+      res = []
+      nums.sort()
+      res.append(nums)
+      index = 0
+      while index < len(res):
+        if len(res[index]) == 1:
+          index += 1
+          continue
+        arr = res[index]
+        for num in arr:
+          tmp = []
+          for i in arr:
+            #if i != num:
+            tmp.append(i)
+            if len(tmp) <= 2:
+              tmp.sort()
+          if tmp not in res:
+            res.append(tmp)
+        index += 1
+      if [] not in res:
+        res.append([])
+      res.sort()
+      return res
+
+
+A = [1, 2, 2]
+print(Solution.subsetsWithDup(A, A))
